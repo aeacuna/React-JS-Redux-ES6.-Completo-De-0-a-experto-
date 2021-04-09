684438593be36b0b098bd6e05734d59ec0ad386d
aeacuna <39201875+aeacuna@users.noreply.github.com>
2021-04-09 10:31:21 -0300

    Update App.jsx

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,32 +1,34 @@
+import React from 'react';
 import './App.css';
 
 const App = () => {
-    return (<main className="react-calculator">
-        <div className="result"></div>
-        <div className="numbers">
-            <buttons>1</buttons>
-            <buttons>2</buttons>
-            <buttons>3</buttons>
-            <buttons>4</buttons>
-            <buttons>5</buttons>
-            <buttons>6</buttons>
-            <buttons>7</buttons>
-            <buttons>8</buttons>
-            <buttons>9</buttons>
-            <buttons>0</buttons>
-        </div>
-        <div className="functions">
-            <buttons>Clear</buttons>
-            <buttons>Remove</buttons>
-        </div>
-        <div className="math-operations">
-            <buttons>+</buttons>
-            <buttons>-</buttons>
-            <buttons>*</buttons>
-            <buttons>/</buttons>
-            <buttons>=</buttons>
-        </div>
-    </main>);
+    return (
+        <main className="react-calculator">
+            <div className="result"></div>
+                <div className="numbers">
+                    <button>1</button>
+                    <button>2</button>
+                    <button>3</button>
+                    <button>4</button>
+                    <button>5</button>
+                    <button>6</button>
+                    <button>7</button>
+                    <button>8</button>
+                    <button>9</button>
+                    <button>0</button>
+                </div>
+                <div className="functions">
+                    <button>Clear</button>
+                    <button>Remove</button>
+                </div>
+                <div className="math-operations">
+                    <button>+</button>
+                    <button>-</button>
+                    <button>*</button>
+                    <button>/</button>
+                    <button>=</button>
+                </div>
+            </main>)
 }
 
 export default App;
