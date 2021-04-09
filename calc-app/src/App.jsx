0141620d8f90a6f7cbb19fadffd838e2ed1da867
aeacuna <39201875+aeacuna@users.noreply.github.com>
2021-04-09 10:05:59 -0300

    Agrego Css

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -1,5 +1,32 @@
+import './App.css';
+
 const App = () => {
-    return <h1>hola mundo</h1>
+    return (<main className="react-calculator">
+        <div className="result"></div>
+        <div className="numbers">
+            <buttons>1</buttons>
+            <buttons>2</buttons>
+            <buttons>3</buttons>
+            <buttons>4</buttons>
+            <buttons>5</buttons>
+            <buttons>6</buttons>
+            <buttons>7</buttons>
+            <buttons>8</buttons>
+            <buttons>9</buttons>
+            <buttons>0</buttons>
+        </div>
+        <div className="functions">
+            <buttons>Clear</buttons>
+            <buttons>Remove</buttons>
+        </div>
+        <div className="math-operations">
+            <buttons>+</buttons>
+            <buttons>-</buttons>
+            <buttons>*</buttons>
+            <buttons>/</buttons>
+            <buttons>=</buttons>
+        </div>
+    </main>);
 }
 
 export default App;
