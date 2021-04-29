028858570ec648cf252d96b3a909a49299cfa007
aeacuna <39201875+aeacuna@users.noreply.github.com>
2021-04-29 18:00:12 -0300

    Update App.jsx

--- calc-app/src/App.jsx
+++ calc-app/src/App.jsx
@@ -5,22 +5,6 @@ import Result from './components/Result'
 import './App.css';
 import { useState } from 'react';
 
-const onClickOperationFunction = operation => (
-    console.log("operation :",operation)
-)
-
-const onClickEqualFunction = equal => (
-    console.log("equal :",equal)
-)
-
-const onContentClearFunction = clear => (
-    console.log("Content Clear")
-)
-
-const onDeleteFunction = del => {
-    console.log("Content Delete")
-}
-
 const App = () => {
     const [stack, setStack] = useState("")
     //const texto = arrayTextoFuncionModificaTexto[0]
@@ -32,14 +16,27 @@ const App = () => {
                 <Numbers 
                     onClickNumber={number => {
                         console.log(number)
-                        setStack(number)
+                        setStack(`${stack}${number}`)
                     }} />
             <Function  
-                onContentClear={onContentClearFunction}
-                onDelete={onDeleteFunction}/>
+                onContentClear={() => {
+                    console.log("Content clear")
+                    setStack(' ')
+                }}
+                onDelete={() => {
+                    console.log("Content Delete")
+                    const newStack = stack.substring(0, stack.length-1)
+                    setStack(newStack)
+                }}/>
             <MathOperations 
-                onClickOperation={onClickOperationFunction}
-                onClickEqual={onClickEqualFunction}/>
+                onClickOperation={operation => {
+                    console.log("operation :",operation)
+                    setStack(`${stack}${operation}`)
+                }}
+                onClickEqual={equal => {
+                    console.log("equal :",equal)
+                    setStack(`${stack}${equal}`)
+                }}/>
         </main>)
 }
 
