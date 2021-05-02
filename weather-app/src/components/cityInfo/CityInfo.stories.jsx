3ccaa416295c7dcdcd9f63149058b791e56aacde
aeacuna <39201875+aeacuna@users.noreply.github.com>
2021-05-02 13:19:05 -0300

    Subo weather-app

---
+++ weather-app/src/components/cityInfo/CityInfo.stories.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+import CityInfo from './CityInfo'
+
+export default {
+    title : "CityInfo",
+    component : CityInfo
+}
+
+export const CityExample = () => (<CityInfo  city="Buenos Aires" country="Argentina"/>)
