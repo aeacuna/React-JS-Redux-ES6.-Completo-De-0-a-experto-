16c50c35a1582af71316020104a670221fd459f1
aeacuna <39201875+aeacuna@users.noreply.github.com>
2021-05-12 19:04:59 -0300

    Inicio Redux - createStore y provider

--- todo-list/src/App.jsx
+++ todo-list/src/App.jsx
@@ -1,14 +1,22 @@
-
 import TodoList from './components/TodoList'
 import TodoInput from './components/TodoInput'
 import './App.css';
+import { createStore } from 'redux' 
+import { Provider } from 'react-redux';
+
+const reducer = (state, action) => {
+  return state
+}
+const store = createStore(reducer)
 
 function App() {
   return (
+    <Provider store={store}>
     <div className="App">
       <TodoInput></TodoInput>
       <TodoList></TodoList>
     </div>
+    </Provider>
   );
 }
 
